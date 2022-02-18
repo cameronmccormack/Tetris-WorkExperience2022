88204e8ab9725216cb7f0c05c0ff65ee85e7d26d
Cameron McCormack <cameron.mccormack@softwire.com>
2022-02-18 12:00:04 +0000

    add stub leaderboard get endpoint

--- backend/src/api/apiRouter.js
+++ backend/src/api/apiRouter.js
@@ -2,10 +2,12 @@ import { ApiRouterBuilder } from './apiRouterBuilder.js'
 import { exampleEndpoint } from './endpoints/exampleEndpoint.js'
 import { postScoreEndpoint } from './endpoints/postScoreEndpoint.js'
 import { signUpEndpoint } from './endpoints/signUpEndpoint.js';
+import { leaderboardEndpoint } from './endpoints/leaderboardEndpoint.js';
 
 const routerBuilder = new ApiRouterBuilder();
 routerBuilder.addGetEndpoint('/', exampleEndpoint);
 routerBuilder.addPostEndpoint('/postScore', postScoreEndpoint);
 routerBuilder.addPostEndpoint('/signUp', signUpEndpoint);
+routerBuilder.addGetEndpoint('/leaderboard', leaderboardEndpoint);
 
 export const apiRouter = routerBuilder.router
