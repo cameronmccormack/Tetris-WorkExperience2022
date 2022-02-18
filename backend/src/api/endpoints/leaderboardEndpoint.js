cebda24e47dee27519c894c8a9314885873c70b5
mo13561 <91287581+mo13561@users.noreply.github.com>
2022-02-18 14:02:17 +0000

    Update leaderboardEndpoint.js

--- backend/src/api/endpoints/leaderboardEndpoint.js
+++ backend/src/api/endpoints/leaderboardEndpoint.js
@@ -2,6 +2,7 @@ import ScoreModel from '../models/scoreModel.js';
 
 export const leaderboardEndpoint = async (req) => {
     const allScores = await ScoreModel.find({});
+    console.log(allScores);
     return {
         scores: allScores
     };
