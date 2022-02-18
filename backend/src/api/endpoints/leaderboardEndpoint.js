e4ba4852a054bdd9193c7931bc1f31beff70b5b8
mo13561 <91287581+mo13561@users.noreply.github.com>
2022-02-18 13:56:58 +0000

    Update leaderboardEndpoint.js

--- backend/src/api/endpoints/leaderboardEndpoint.js
+++ backend/src/api/endpoints/leaderboardEndpoint.js
@@ -1,12 +1,8 @@
-// This is a stub endpoint created to test pulling data through from the front end
-export const leaderboardEndpoint = (req, res) => {
+import ScoreModel from '../models/scoreModel.js';
+
+export const leaderboardEndpoint = async (req) => {
+    const allScores = await ScoreModel.find({});
     return {
-        scores: [
-            { name: 'Best Player', score: 125000 },
-            { name: 'Another Player', score: 110000 },
-            { name: 'Player 3', score: 98000 },
-            { name: 'Fourth Person', score: 50000 },
-            { name: 'Final Player', score: 40000 }
-        ]
+        scores: allScores
     };
 }
